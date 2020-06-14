3d23e49191b19941f428548a42215676a7a84fed
Jodham Wanjala <jodham>
2020-06-14 13:13:30 +0300

    first commit

---
+++ homesite.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>mysite</title>
+	<link rel="stylesheet" type="text/css" href="mysite.css">
+</head>
+<body>
+<nav class="navigation">
+	<div class="menu_bar flex_row">
+		<div class="site_brand">
+		<a href="#" class="site_name">FanFun.com</a>
+	</div>
+	<div class="toggle-collapse">
+		<div>
+			<i class="bars"></i>
+		</div>
+	</div>
+	<div>  
+		<ul class="menu_items">
+			<li class="menu_link">
+				<a href="">HOME</a>
+			</li>
+			<li class="menu_link">
+				<a href="">FOOTBALL</a>
+			</li>
+			<li class="menu_link">
+				<a href="">MEMES</a>
+			</li>
+			<li class="menu_link">
+				<a href="">BIRTHDAY HACK</a>
+			</li>
+
+		</ul>
+	</div>
+	</div> 
+</nav> 
+<!------------------main site section------>
+<main>
+	<!----------------site title----------->
+	<section class="site_title">
+		<div class="site_background">
+			<h3>Fanfun</h3>
+			<h1>jodham wanjala</h1>
+			<button class="btn">explore</button>
+		</div>
+	</section>
+	<!--------x--------site title-----x------>
+</main>
+<!--------x----------main site section--x---->
+
+<script type="text/javascript" src="mysite.js"></script>
+</body>
+</html>
